c2a7b7733019c4c15de2e3582f54280496397e44
Hugo Chavar <hechavar@gmail.com>
2025-04-12 22:22:43 -0300

    Fix

--- message_processor.py
+++ message_processor.py
@@ -1,4 +1,4 @@
-from policy_data import load_csv_data, filter_data
+from policy_data import load_csv_data, apply_filter
 from ai_agents import generate_query, generate_response
 
 def generate_response(incoming_message: str) -> str:
@@ -15,5 +15,5 @@ def generate_response(incoming_message: str) -> str:
     incoming_message = incoming_message.lower()
     load_csv_data()
     query = generate_query(incoming_message)
-    filtered_data = filter_data(query)
+    filtered_data = apply_filter(query)
     return generate_response(filtered_data)
