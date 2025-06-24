7c1c8a248709373e4bce0990006a80b20d96a916
Hugo Chavar <hechavar@gmail.com>
2025-06-24 11:56:28 -0300

    Save results

--- download_policies.py
+++ download_policies.py
@@ -10,7 +10,8 @@ from policy_driver import PolicyDriver
 logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s - %(levelname)s - %(message)s",
-    handlers=[logging.StreamHandler(sys.stdout)],  # Explicitly use stdout
+    filename="sura_downloader_test1.log",
+    # handlers=[logging.StreamHandler(sys.stdout)],  # Explicitly use stdout
 )
 
 logger = logging.getLogger(__name__)
@@ -21,9 +22,13 @@ load_csv_data()
 
 policy_data = get_grouped_policy_data()
 
-# with open("policy_data5.json", encoding="utf-8", mode="w") as c:
+# with open("all_policy_data.json", encoding="utf-8", mode="w") as c:
 #     r = json.dumps(policy_data, indent=2, ensure_ascii=False)
 #     c.write(r)
+
+results = []
+with open("policy_results2.json", encoding="utf-8", mode="r") as c:
+    results = json.load(c)
 
 sura_downloader = SuraDownloader(PolicyDriver(headless=False))
 
@@ -33,11 +38,18 @@ for company, policies in policy_data.items():
     if company != "SURA":
         continue
     random_items = random.sample(policies, min(3, len(policies)))
+    with open("policy_input.json", encoding="utf-8", mode="w") as c:
+        r = json.dumps(random_items, indent=2, ensure_ascii=False)
+        c.write(r)
+    ## remove already downloaded policies
+    existing_policy_numbers = [p["number"] for p in results]
+    random_items = [
+        item for item in random_items if item["number" not in existing_policy_numbers]
+    ]
     sura_downloader.process_policies(random_items)
     for policy in random_items:
         # TODO: design database/update database
-        # print(f"  Policy: {policy['number']}, Expires: {policy['expiration_date']}")
-        # expiration_year = policy['expiration_date'].split('/')[-1]
+        results.append(policy)
         try:
             if not policy["obs"]:
                 for car in policy["vehicles"]:
@@ -49,3 +61,8 @@ for company, policies in policy_data.items():
         except Exception as e:
             print(f"Error in: {policy}")
             print(str(e))
+
+    results_sorted = sorted(results, key=lambda policy: policy["number"])
+    with open("policy_results.json", encoding="utf-8", mode="w") as c:
+        r = json.dumps(results, indent=2, ensure_ascii=False)
+        c.write(r)
