f7dffb3dbee2c1375d446a235cce6fdd930355e3
Hugo Chavar <hechavar@gmail.com>
2025-06-28 15:20:08 -0300

    Cleanup and add logs

--- download_policies.py
+++ download_policies.py
@@ -21,25 +21,11 @@ from sura_downloader import SuraDownloader
 from policy_driver import PolicyDriver
 
 
-def log_policy(company, policy):
-    try:
-        if not policy["obs"]:
-            for car in policy["vehicles"]:
-                print(
-                    f"{company}/{policy['number']}/{car['license_plate']}/{policy['year']}"
-                )
-        else:
-            print(f"{company}/{policy['number']}/{policy["obs"]}")
-    except Exception as e:
-        print(f"Error in: {policy}")
-        print(str(e))
-        logging.exception("Detailed error:")
-
-
 def need_to_be_processed(company, policy):
     policy_db = db.get_policy_with_cars(company, policy["number"])
     policy["db"] = policy_db
     if not policy_db:
+        logger.info(f"Policy {policy["number"]} not in DB")
         return True
 
     new_policy_exp_date = datetime.strptime(
@@ -47,11 +33,14 @@ def need_to_be_processed(company, policy):
     ).date()
 
     if new_policy_exp_date > policy_db.expiration_date:
+        logger.info(f"Policy {policy["number"]} has new expiration date")
         return True
 
     if not policy_db.downloaded:
+        logger.info(f"Policy {policy["number"]} already downloaded")
         return True
 
+    logger.info(f"Policy {policy["number"]} already PROCESSED")
     return False
 
 
@@ -118,32 +107,12 @@ for company, policies in policy_data.items():
     if kept_policies:
         new_policy_data[company] = kept_policies
 
-results = []
-with open("policy_results.json", encoding="utf-8", mode="r") as c:
-    results = json.load(c)
 
 sura_downloader = SuraDownloader(PolicyDriver(headless=False))
 
 for company, policies in new_policy_data.items():
     if company != "SURA":
         continue
-    # random_items = random.sample(policies, min(3, len(policies)))
-    random_items = [i for i in policies if i["number"] == "2121199"]
-    with open("policy_input.json", encoding="utf-8", mode="w") as c:
-        r = json.dumps(random_items, indent=2, ensure_ascii=False)
-        c.write(r)
-    ## remove already downloaded policies
-    existing_policy_numbers = [p["number"] for p in results]
-    random_items = [
-        item for item in random_items if item["number"] not in existing_policy_numbers
-    ]
-    sura_downloader.process_policies(random_items)
-    for policy in random_items:
-        results.append(policy)
-        log_policy(company, policy)
-        insert_processed_policies(company, random_items)
 
-    results_sorted = sorted(results, key=lambda policy: policy["number"])
-    with open("policy_results.json", encoding="utf-8", mode="w") as c:
-        r = json.dumps(results, indent=2, ensure_ascii=False)
-        c.write(r)
+    sura_downloader.process_policies(policies)
+    insert_processed_policies(company, policies)
